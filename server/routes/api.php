5de3ea2c07017b465e715bc5f5344098a7f70b14
DhaniXI <ramabmp1@gmail.com>
2023-06-09 17:44:48 +0800

    test server folder

--- server/routes/api.php
+++ server/routes/api.php
@@ -26,6 +26,32 @@ Route::get('/users', function () {
     return $users;
 });
 
+Route::post('/users', function (Request $request) {
+    // Validasi input dari request
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'phone_number' => 'required|unique:users',
+        'password' => 'required|min:6',
+    ]);
+
+    // Membuat objek User baru
+    $user = new User;
+    $user->name = $request->input('name');
+    $user->email = $request->input('email');
+    $user->phone_number = $request->input('phone_number');
+    $user->password = bcrypt($request->input('password'));
+
+    // Menyimpan data pengguna ke database
+    $user->save();
+
+    // Mengembalikan response sukses dengan data pengguna yang baru ditambahkan
+    return response()->json([
+        'message' => 'User created successfully',
+        'user' => $user
+    ], 201);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
