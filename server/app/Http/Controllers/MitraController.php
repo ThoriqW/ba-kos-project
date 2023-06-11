a753be220bdb011c5bfeaa24bbc7d8bf18babb5f
DhaniXI <ramabmp1@gmail.com>
2023-06-11 19:41:56 +0800

    test server folder

--- server/app/Http/Controllers/MitraController.php
+++ server/app/Http/Controllers/MitraController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class MitraController extends Controller
 {
 
+    public function index()
+    {
+        $user = User::all();
+        return response()->json($user);
+    }
+
     public function store(Request $request)
     {
         // Validasi input dari request
@@ -15,7 +21,7 @@ class MitraController extends Controller
             'name' => 'required|min:3|max:255',
             'email' => 'required|email:dns|unique:users',
             'phone_number' => 'required|unique:users|regex:/^08\d{8,}$/i',
-            'password' => 'required|min:8|confirmed'
+            'password' => 'required|min:8'
         ]);
 
         // Membuat objek User baru
--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
 
+    public function index()
+    {
+        $user = User::all();
+        return response()->json($user);
+    }
+
     public function store(Request $request)
     {
         // Validasi input dari request
