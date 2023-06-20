37f44a48644708cd723253aeaac298f5d7362acc
DhaniXI <ramabmp1@gmail.com>
2023-06-21 00:13:41 +0800

    data user login

--- server/app/Http/Controllers/MitraController.php
+++ server/app/Http/Controllers/MitraController.php
@@ -97,6 +97,7 @@ class MitraController extends Controller
             return response()->json([
                 'status' => true,
                 'message' => 'User Logged In Successfully',
+                'user' => $user->load('kos'), // Memuat data profil terkait
                 'token' => $user->createToken("API TOKEN")->plainTextToken
             ], 200);
         } catch (\Throwable $th) {
